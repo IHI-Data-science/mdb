87aed60f6c97042c0cf72304f846926d043ff812
Gkiwelu <80961079+Gkiwelu@users.noreply.github.com>
2021-07-31 19:16:29 +0300

    updated version for ss2

--- app_backend/includes/functions.php
+++ app_backend/includes/functions.php
@@ -246,7 +246,7 @@ function ss1data(
 
 
 
-function ss2data($sen, $ssen, $sfr, $hf, $htc, $htr, $hw, $hs, $fr, $tx, $bf, $pu, $vl, $vt, $ndi01, $ndi02, $ndi03, $ndi04, $ndi05, $ndi06, $ndi07, $ndi08, $ndi09, $ndi10, $n, $notes, $slc, $ni, $nb, $sid01, $sid02, $sid03, $sid04, $sid05, $sid06, $sid07, $nd)
+function ss2data($sen, $ssen, $sfr, $hf, $hbt,$htc, $htr, $hw, $hs,$dnh, $fr, $tx, $bfegg,$bfl1,$bfl2,$bfl3,$bfl4,$l1l2,$l3l4,$tlv, $pu, $vl,$rl, $vt,$no, $notes, $slc, $ni, $nb, $sid01, $sid02, $sid03, $sid04, $sid05, $sid06, $sid07, $nod)
 {
     global $db;
   
@@ -291,8 +291,11 @@ function ss2data($sen, $ssen, $sfr, $hf, $htc, $htr, $hw, $hs, $fr, $tx, $bf, $p
         $senfr = $sen.$fr;
     }
 
-    $resultft = $db->mysqliquery("INSERT INTO  $ss1 ( edssid, sen,ssen,sfr,hw,hs,fr,tx,bf,ndi01,ndi02,ndi03,ndi04,ndi05,ndi06,ndi07,ndi08,ndi09,ndi10,n,notes,slc,st,ni,nb,sid01,sid02,sid03,sid04,sid05,sid06,sid07,nd,senfr)
-    VALUES('$edssid','$sen','$ssen','$sfr','$hw','$hs','$fr','$tx','$bf','$ndi01','$ndi02','$ndi03','$ndi04','$ndi05','$ndi06','$ndi07','$ndi08','$ndi09','$ndi10','$n','$notes','$slc','$st','$ni','$nb','$sid01','$sid02','$sid03','$sid04','$sid05','$sid06','$sid07','$nd','$senfr')");
+    $resultft = $db->mysqliquery("INSERT INTO  $ss2 ( edssid, sen,ssen,sfr,hf,hbt,htc,htr,hw,hs,dnh,fr,tx,bfegg,bfl1,bfl2,bfl3,bfl4,l1l2,l3l4,tlv,no,notes,slc,ni,nb,sid01,sid02,sid03,sid04,sid05,sid06,sid07,nod,senfr)
+    VALUES('$edssid','$sen','$ssen','$sfr','$hf','$hbt','$htc',$htr','$hw','$hs,'$dnh','$fr','$tx','$bfegg','$bfl1','$bfl2','$bfl3','$bfl4','$l1l2',$l3l4,'$tlv,$pu','$vl','$rl','$vt','$no','$notes','$slc','$ni','$nb','$sid01','$sid02','$sid03','$sid04','$sid05','$sid06','$sid07','$nod','$senfr')");
+
+
+//f2data
 
         
     if ($resultft) {
