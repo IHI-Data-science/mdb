914645ae9527e986cf79eaaa54010e3d4dacb46f
NjireC <irenechoba@gmail.com>
2021-08-25 12:48:08 +0300

    config file and ss3graphs

--- includes/config.php
+++ includes/config.php
@@ -11,11 +11,11 @@ define( 'DB_NAME', 'iebsweb5' );
 define( 'URL', 'http://mosquitodb.io/mdb' );
 
 
-define( 'DB_HOST', 'localhost' );          
-define( 'DB_USER', 'root' );             
-define( 'DB_PASS', '' );            
-define( 'DB_NAME', 'mdb' );       
-define( 'URL', 'http://localhost:81/mdb' ); 
+// define( 'DB_HOST', 'localhost' );          
+// define( 'DB_USER', 'root' );             
+// define( 'DB_PASS', '' );            
+// define( 'DB_NAME', 'mdb' );       
+// define( 'URL', 'http://localhost:81/mdb' ); 
 
 
 
